198ad227733ac3d94b834460caeb8d81dfb5b533
nataekyun <bbsmax@nate.com>
2019-12-31 05:40:13 +0900

    설정파일 추가

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const path = require('path');
 const flash = require('connect-flash'); //웹브라우저에 1회성 메세지를 만들때.
 const app = express();
+require('dotenv').config();
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,11 +15,11 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(cookieParser('nodebirdsecret'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave : false,
     saveUninitialized : false,
-    secret : 'nodebirdsecret',
+    secret : process.env.COOKIE_SECRET,
     cookie : {
         httpOnly : true,
         secure : false,
@@ -28,5 +29,5 @@ app.use(session({
 app.use(flash());
 
 app.listen(app.get('port'), () => {
-    console.log(`8001번 포트에서 서버가 실행중입니다.`);
+    console.log(`${app.get('port')}번 포트에서 서버가 실행중입니다.`);
 })
