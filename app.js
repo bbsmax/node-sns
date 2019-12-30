60a0a5e9d1aedad81df7f354c9a12a2bd0bbdf94
nataekyun <bbsmax@nate.com>
2019-12-31 05:30:20 +0900

    node sns

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const path = require('path');
+const flash = require('connect-flash'); //웹브라우저에 1회성 메세지를 만들때.
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser('nodebirdsecret'));
+app.use(session({
+    resave : false,
+    saveUninitialized : false,
+    secret : 'nodebirdsecret',
+    cookie : {
+        httpOnly : true,
+        secure : false,
+    },
+}));
+
+app.use(flash());
+
+app.listen(app.get('port'), () => {
+    console.log(`8001번 포트에서 서버가 실행중입니다.`);
+})
